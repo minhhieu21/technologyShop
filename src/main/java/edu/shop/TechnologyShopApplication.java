8141ce127fd37bd90b85b7359402a9ff6ce95161
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-05 10:00:07 +0700

    init project

---
+++ src/main/java/edu/shop/TechnologyShopApplication.java
@@ -0,0 +1,13 @@
+package edu.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/shop/TechnologyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
