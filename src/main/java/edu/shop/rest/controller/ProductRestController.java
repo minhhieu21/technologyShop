727b948495495bdb13ca3c4fc81a1523ef2cc154
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-05 23:30:33 +0700

    add file productRestController

---
+++ src/main/java/edu/shop/rest/controller/ProductRestController.java
@@ -0,0 +1,25 @@
+package edu.shop.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.shop.entity.Product;
+import edu.shop.service.ProductService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/rest/products")
+public class ProductRestController {
+	
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping("{id}")
+	public Product getOneProduct(@PathVariable("id") Integer id) {
+		return productService.getById(id);
+	}
+}
