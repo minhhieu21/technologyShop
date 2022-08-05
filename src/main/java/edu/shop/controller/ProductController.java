403d16b271b7f4fe1614beeab3af57182e4e6518
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-05 12:54:01 +0700

    disble  spring security and edit code

--- src/main/java/edu/shop/controller/ProductController.java
+++ src/main/java/edu/shop/controller/ProductController.java
@@ -1,12 +1,15 @@
 package edu.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("product")
 public class ProductController {
-	@RequestMapping("product/list")
 	
+	
+	@GetMapping("list")
 	public String list(){
 		return "product/list";
 	}
