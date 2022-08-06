4369b08c3326b350948cd53528376517123f0122
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-06 10:14:04 +0700

    security Webconfig

--- src/main/java/edu/shop/config/InterceptorConfig.java
+++ src/main/java/edu/shop/config/InterceptorConfig.java
@@ -1,4 +1,4 @@
-package edu.shop;
+package edu.shop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/edu/shop/service/AccountService.java
@@ -0,0 +1,15 @@
+package edu.shop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import edu.shop.entity.Category;
+
+public interface AccountService {
+
+}
